7c1efb60c3d59b407959da0ea70492b49f1c9061
NicholasShewfelt <151573246+NicholasShewfelt@users.noreply.github.com>
2024-06-29 18:56:57 -0500

    Update train_model.py
    Updates train_model.py

--- train_model.py
+++ train_model.py
@@ -12,20 +12,18 @@ from ml.model import (
     save_model,
     train_model,
 )
-# TODO: load the cencus.csv data
+
 #project_path = os.getcwd()
 data_path = os.path.join( "data", "census.csv")
 print(data_path)
 data = pd.read_csv(data_path)
 
-# TODO: split the provided data to have a train dataset and a test dataset
-# Optional enhancement, use K-fold cross validation instead of a train-test
-# split.
+#splits the data to have a train dataset and a test dataset
 train, test = train_test_split(data,
                                test_size=0.15,
                                random_state=42)
 
-# DO NOT MODIFY
+#hard codes the catagorical features
 cat_features = [
     "workclass",
     "education",
@@ -37,7 +35,7 @@ cat_features = [
     "native-country",
 ]
 
-# TODO: use the process_data function provided to process the data.
+#uses the process_data function provided to process the data.
 X_train, y_train, encoder, lb = process_data(
     train,
     categorical_features=cat_features,
@@ -54,30 +52,30 @@ X_test, y_test, _, _ = process_data(
     lb=lb,
 )
 
-# TODO: use the train_model function to train the model on the training dataset
+#uses the train_model function to train the model on the training dataset
 model = train_model(X_train, y_train)
 
-# save the model and the encoder
+#saves the model and the encoder
 model_path = os.path.join( "model", "model.pkl")
 save_model(model, model_path)
 encoder_path = os.path.join( "model", "encoder.pkl")
 save_model(encoder, encoder_path)
 
-# load the model
+#loads the model
 model = load_model(
     model_path
 )
 
-# TODO: use the inference function to run the model inferences on the test
-# dataset.
+#uses the inference function to run the model inferences on the test
+# dataset
 preds = inference(model, X_test)
 
-# Calculate and print the metrics
+# Calculates and print the metrics
 p, r, fb = compute_model_metrics(y_test, preds)
 print(f"Precision: {p:.4f} | Recall: {r:.4f} | F1: {fb:.4f}")
 
-# TODO: compute the performance on model slices using the
-# performance_on_categorical_slice function
+#computes the performance on model slices using the
+# performance_on_categorical_slice function to
 # iterate through the categorical features
 for col in cat_features:
     # iterate through the unique values in one categorical feature
